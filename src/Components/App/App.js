d2cd80385c21864d7f6304213ce6e0ff59e2d08c
Pierson Brannan <pbrannan@blackwillow.net>
2019-09-27 10:46:35 -0400

    Init projet setup

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state={}
+    }
+
+    render() {
+        return(
+            <div className="App">
+            </div>
+        )
+    }
+}
+
+export default App;
