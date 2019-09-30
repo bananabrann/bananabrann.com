6f6fd38a1b547a5f9f9237c23793d31500b2d5e5
Pierson Brannan <pbrannan@blackwillow.net>
2019-09-30 10:57:13 -0400

    Add social media mini-footer to homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import bananaPicture from "../../img/bananas.png";
+import linkedInPicture from "../../img/linkedin.png";
 import "./Home.css";
 
 const DOCUMENT_TITLE = "Bananabrann - Home";
@@ -16,23 +17,29 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <div className="home-left-area">
-          <img alt="" src={bananaPicture} />
+        <div className="content-wrapper">
+            <div className="home-left-area">
+            <img alt="" src={bananaPicture} />
+            </div>
+
+            <div className="home-right-area">
+            <Link to={RESUME_PATH}>
+                <div className="home-nav-box-area">
+                <h2>Resume</h2>
+                <p>View or download resume</p>
+                </div>
+            </Link>
+            <Link to={LABS_PATH}>
+                <div className="home-nav-box-area">
+                <h2>Lab</h2>
+                <p>View projects and works</p>
+                </div>
+            </Link>
+            </div>
         </div>
 
-        <div className="home-right-area">
-          <Link to={RESUME_PATH}>
-            <div className="home-nav-box-area">
-              <h2>Resume</h2>
-              <p>View or download resume</p>
-            </div>
-          </Link>
-          <Link to={LABS_PATH}>
-            <div className="home-nav-box-area">
-              <h2>Lab</h2>
-              <p>View projects and works</p>
-            </div>
-          </Link>
+        <div className="home-social-media-wrapper">
+            <a href="https://www.linkedin.com/in/pbrann/"><img id="linkedin-logo" alt="Follow me on LinkedIn" src={linkedInPicture} /></a>
         </div>
       </div>
     );
