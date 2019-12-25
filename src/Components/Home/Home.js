c51c60df22d2611fe18ff66643b90cd04bd5cf99
Pierson Brannan <plbrannan95@gmail.com>
2019-12-25 09:11:16 -0600

    Remove bubble animation js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Bubble from "./Bubble/Bubble";
 import bananaPicture from "../../img/bananas.png";
 import linkedInPicture from "../../img/linkedin.png";
 import "./Home.css";
@@ -19,33 +18,6 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        {/* <div className="content-wrapper">
-          <div className="home-left-area">
-            <img alt="" src={bananaPicture} />
-          </div>
-
-          <div className="home-right-area">
-            <Link to={RESUME_PATH}>
-              <div className="home-nav-box-area">
-                <h2>Resume</h2>
-                <p>View or download resume</p>
-              </div>
-            </Link>
-            <Link to={LABS_PATH}>
-              <div className="home-nav-box-area">
-                <h2>Lab</h2>
-                <p>View projects and works</p>
-              </div>
-            </Link>
-          </div>
-        </div>
-
-        <div className="home-social-media-wrapper"> */}
-
-          <div className="bubblegum-circle">
-            <Bubble />
-            </div>
-
           {/* prettier-ignore */}
           <a href="https://www.linkedin.com/in/pbrann/"><img id="linkedin-logo" alt="Follow me on LinkedIn" src={linkedInPicture} /></a>
       </div>
