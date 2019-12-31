5a4d4bf1bbe6c6a6bc1e6c6835a5331fc9b253ee
Pierson Brannan <plbrannan95@gmail.com>
2019-12-31 11:27:22 -0600

    Add g-tv logos, css secondary tiles

--- src/Components/GrandparentsTV/GrandparentsTV.js
+++ src/Components/GrandparentsTV/GrandparentsTV.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./GrandparentsTV.css";
+import picGMaps from "../../img/g-tv/google-maps.png";
+import picNetflix from "../../img/g-tv/netflix.png";
+import picWikipedia from "../../img/g-tv/wikipedia.png";
+import picYoutube from "../../img/g-tv/youtube.png";
 
 const DOC_TITLE = "Brannan TV";
 
@@ -17,13 +21,14 @@ class GrandparentsTV extends React.Component {
       <div id="g-tv">
         <p>Hello grandma and grandpa!</p>
         <div id="g-tv-main-area">
-            <div className="tile" id="tile-youtube">Youtube</div>
-            <div className="tile" id="tile-youtube">Netflix</div>
+            <div className="tile" id="tile-youtube"><h4>Youtube</h4></div>
+            <div className="tile" id="tile-netflix"><h4>Netflix</h4></div>
         </div>
         <div id="g-tv-secondary-area">
-            <div className="tile" id="tile-wikipedia">Wiki</div>
-            <div className="tile" id="tile-google-maps">Google Maps</div>
+            <div className="tile" id="tile-wikipedia"><h5>Wikipedia</h5></div>
+            <div className="tile" id="tile-google-maps"><h5>Google Maps</h5></div>
         </div>
+        <div id="g-tv-help">Help Me!</div>
       </div>
     );
   }
