6aec526df320b45363552e5120ca4e8f5b33531a
Pierson Brannan <pbrannan@blackwillow.net>
2020-01-06 09:18:16 -0500

    Dynamic directory path

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 # GREEN = "\e[32m"
 # BOLD = "\e[1m"
 # CLEAR="\033[0m"
-
-npm run-script build
-aws s3 sync /Users/pi/Dev/bananabrann.com/build s3://bananabrann.com
+DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+npm run-script build && aws s3 sync $DIR/build s3://bananabrann.com
 
 printf "\n\e[32m\e[1mBuild and deploy sucessful!\033[0m\n"
