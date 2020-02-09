6599be768b194948d2cb9c7328731ace92348494
Pierson Brannan <plbrannan95@gmail.com>
2020-02-09 10:44:15 -0500

    Zoom in and zoom out functionality

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -9,6 +9,35 @@ const DOCUMENT_TITLE = "bananabrann - Resume";
 const HOME_PATH = "/";
 
 class Resume extends React.Component {
+  constructor() {
+    super()
+    this.zoomIn = this.zoomIn.bind(this)
+    this.zoomOut = this.zoomOut.bind(this)
+  }
+
+  zoomIn() {
+    let myImg = document.getElementById("resume")
+    let currWidth = myImg.clientWidth
+    console.log(currWidth)
+    if(currWidth > 1500) {
+      alert("Maximum zoom-in level reached!")
+    }
+     
+    myImg.style.width = (currWidth + 50) + "px"
+    
+  }
+
+  zoomOut() {
+    let myImg = document.getElementById("resume")
+    let currWidth = myImg.clientWidth
+    console.log(currWidth)
+    if(currWidth > 1500) {
+      alert("Maximum zoom-out level reached!")
+    } else {
+      myImg.style.width = (currWidth -50) + "px"
+    }
+  }
+
   render() {
     return (
       <div className="Resume">
@@ -21,10 +50,11 @@ class Resume extends React.Component {
           <button><a href={resumeTXT} download>Download TXT</a></button>
         </div>
         <div className="zoom-buttons">
-          <button id="zoom-in" type="button" onclick="zoomIn()"><i class="fas fa-search-plus"></i></button>
-          <button id="zoom-out" type="button" onclick="zoomOut()"><i class="fas fa-search-minus"></i></button>
+          <button id="zoom-in" type="button" onClick={() => this.zoomIn()}><i class="fas fa-search-plus"></i></button>
+          <button id="zoom-out" type="button" onClick={() => this.zoomOut()}><i class="fas fa-search-minus"></i></button>
         </div>
         <img
+          id="resume"
           alt="Pierson Brannan's resume. Download the resume instead via the 'download' button above"
           src={resumeJPG}
         />
