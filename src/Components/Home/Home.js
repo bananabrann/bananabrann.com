cea84e92573ddc9ec1fde98d6a9c940f069fcb8b
Pierson Brannan <plbrannan95@gmail.com>
2020-02-09 11:15:45 -0500

    Replace Home social media images with Glpyicons

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import linkedInPicture from "../../res/img/linkedin.png";
-import githubLogo from "../../res/img/github.png";
-import stackOverflowLogo from "../../res/img/stackoverflow.png";
 import cubeGif from "../../res/img/cube.gif";
-import techStack from "../../res/img/techstack.png";
 import "./Home.scss";
 
 const DOCUMENT_TITLE = "bananabrann - Home";
@@ -17,21 +13,18 @@ class Home extends React.Component {
           <img src={cubeGif} id="cube" alt="" />
           <p><b>Pierson L. Brannan</b></p>
           <p id="welcome-message"> –– Self-taught software engineer, prior professional tuba player and U.S. Marine.</p>
-          {/* <img src={techStack} id="techstack" alt="" /> */}
         </div>
 
         <div id="lower">
           <a href="https://www.linkedin.com/in/pbrann/">
-            {/* prettier-ignore */}
-            <img className="logo" id="linkedin" alt="Follow me on LinkedIn" src={linkedInPicture} />
+            <i class="fab fa-linkedin-in fa-3x"></i>
           </a>
           <a href="https://www.github.com/bananabrann">
-            {/* prettier-ignore */}
-            <img className="logo" id="github" alt="Visit my GitHub" src={githubLogo} />
+            <i class="fab fa-github-alt fa-3x"></i>
           </a>
-          {/* <a href="https://stackoverflow.com/users/11057120/bananabrann?tab=profile">
-            <img className="logo" id="stackoverflow" alt="See my Stack Overflow profile" src={stackOverflowLogo} />
-          </a> */}
+          <a href="https://stackoverflow.com/users/11057120/bananabrann?tab=profile">
+            <i class="fab fa-stack-overflow fa-3x"></i>
+          </a>
         </div>
       </div>
     );
