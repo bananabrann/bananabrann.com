8271da83e0745b38da8d875145ba739f749cbef2
Pierson Brannan <plbrannan95@gmail.com>
2020-02-22 20:38:32 -0500

    Change app component font

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -32,25 +32,38 @@ class Home extends React.Component {
             <Col>
               <Row>
                 <div id="homepage-header">
-                  <h1>Bananabrannan</h1>
+                  <h1>Brannan</h1>
                   <h2 id="homepage-message"><b></b>{this.state.greetingMessage}</h2> 
                 </div>
               </Row>
-              <Row id="media-links">
-                <a href="https://www.linkedin.com/in/pbrann/">
-                  <i class="fab fa-linkedin-in fa-3x"></i>
-                </a>
-                <a href="https://www.github.com/bananabrann">
-                  <i class="fab fa-github-alt fa-3x"></i>
-                </a>
-                <a href="https://stackoverflow.com/users/11057120/bananabrann?tab=profile">
-                  <i class="fab fa-stack-overflow fa-3x"></i>
-                </a>
-              </Row>
             </Col>
           </Row>
+          
           <Row>
             <Break />
+          </Row>
+
+          <Row>
+            <Col lg={3}>
+              <Row id="media-links">
+                <a href="https://www.linkedin.com/in/pbrann/">
+                  <i class="fab fa-linkedin-in fa-2x"></i>
+                </a>
+                <a href="https://www.github.com/bananabrann">
+                  <i class="fab fa-github-alt fa-2x"></i>
+                </a>
+                <a href="https://stackoverflow.com/users/11057120/bananabrann?tab=profile">
+                  <i class="fab fa-stack-overflow fa-2x"></i>
+                </a>
+              </Row>
+              <Row className="left-nav-item"><p>Resume</p></Row>
+              <Row className="left-nav-item"><p>Lab</p></Row>
+            </Col>
+
+            <Col id="home-content-row">
+              <h3>About</h3>
+              <p>My name is Pierson Brannan, and I'm a self-taught developer, prior U.S. Marine, and tuba player from West Texas.</p>
+            </Col>
           </Row>
         </Container>
       </div>
