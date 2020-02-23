5f5456f144273d8a5861653c9d454a321e0bc417
Pierson Brannan <plbrannan95@gmail.com>
2020-02-23 12:30:04 -0500

    Alert for under construction pages

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import "./Home.scss"
 
+import "./Home.scss"
 import Break from "../Break/Break"
+
+import Alert from "react-bootstrap/Alert"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
@@ -21,16 +23,36 @@ class Home extends React.Component {
     super()
     this.state = {
       greetingMessage: "Self-taught software developer",
-      previousGreetingMessage: null
+      previousGreetingMessage: null,
+      displayAlert: false
     }
-    this.updateRandomGreetingMessage = this.updateRandomGreetingMessage.bind(
-      this
-    )
+    // prettier-ignore
+    this.updateRandomGreetingMessage = this.updateRandomGreetingMessage.bind(this)
+    // prettier-ignore
+    this.handleNotAvailableLinkClick = this.handleNotAvailableLinkClick.bind(this)
   }
 
   render() {
+    let alert
+
+    if (this.state.displayAlert) {
+      alert = (
+        <Alert
+          variant="warning"
+          onClose={() => this.setState({ displayAlert: false })}
+          dismissible
+        >
+          <Alert.Heading><i class="fas fa-wrench"></i> Under construction</Alert.Heading>
+          <p>
+            This section is a Work In Progress, and unavailable at this time. Be sure and come back!
+          </p>
+        </Alert>
+      )
+    }
+
     return (
       <div className="Home">
+        {alert}
         <Container>
           <Row>
             <Col>
@@ -63,26 +85,37 @@ class Home extends React.Component {
                   <i class="fab fa-stack-overflow fa-2x"></i>
                 </a>
               </Row>
-              <Link to={LINK_RESUME}>
-                <Row className="left-nav-item">
-                  <p className="link link-watercourse">Resume</p>
-                </Row>
-              </Link>
+
               <Row className="left-nav-item">
-                <p className="link link-orange">Showcase</p>
+                <Link to={LINK_RESUME}>
+                  <p className="link link-watercourse">Resume</p>
+                </Link>
+              </Row>
+
+              <Row className="left-nav-item">
+                <a onClick={this.handleNotAvailableLinkClick}>
+                  <p className="link not-available">Software Showcase</p>
+                </a>
+              </Row>
+
+              <Row className="left-nav-item">
+                <a onClick={this.handleNotAvailableLinkClick}>
+                  <p className="link not-available">Visit Tuba Profile</p>
+                </a>
               </Row>
             </Col>
 
             <Col id="home-content-row">
               <h3>About</h3>
               <p>
-                I'm <b>Pierson Brannan</b>, and I'm a self-taught developer, prior U.S.
-                Marine, and tuba player from West Texas. I came to Washington
-                D.C. when landing a full-time professional tuba player
-                position with "The Commandant's Own", the United States Marine Drum & Bugle Corps.
-                It was a great experience, performing for some of the America's highest ranking
-                officials and at venues I'll never forget, but I've since left, and have
-                stuck around the Washington area.
+                I'm <b>Pierson Brannan</b>, and I'm a self-taught developer,
+                prior U.S. Marine, and tuba player from West Texas. I came to
+                Washington D.C. when landing a full-time professional tuba
+                player position with "The Commandant's Own", the United States
+                Marine Drum & Bugle Corps. It was a great experience, performing
+                for some of the America's highest ranking officials and at
+                venues I'll never forget, but I've since left, and have stuck
+                around the Washington area.
               </p>
               <br />
               <p>
@@ -97,6 +130,12 @@ class Home extends React.Component {
         </Container>
       </div>
     )
+  }
+
+  handleNotAvailableLinkClick() {
+    this.setState({
+      displayAlert: true
+    })
   }
 
   updateRandomGreetingMessage() {
