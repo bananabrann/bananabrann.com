0ff432d58913e84e206b015d7e9f3624f649cd6d
Pierson Brannan <plbrannan95@gmail.com>
2020-04-18 16:16:10 -0400

    Add base Threejs

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import * as THREE from "three";
 import "./Home.scss";
 
 const LINK_RESUME = "/resume";
@@ -17,6 +18,20 @@ class Home extends React.Component {
         return (
             <div className="Home">
                 <Container fluid>
+                    <Row>
+                        <h1>Pierson Brannan</h1>
+                    </Row>
+
+                    <Row>
+                        <p>
+                            I'm a software developer in the U.S. Before coding,
+                            I was a professional tuba player, performing gigs
+                            internaionally full-time. I was in the U.S. Marines,
+                            and love aquascaping (aquariums with a focus of
+                            landscape and plats)
+                        </p>
+                    </Row>
+
                     <Row id="media-links">
                         <a href="https://www.linkedin.com/in/pbrann/">
                             <i class="fab fa-linkedin-in fa-2x"></i>
@@ -35,6 +50,40 @@ class Home extends React.Component {
 
     componentDidMount() {
         document.title = DOCUMENT_TITLE;
+
+        /*
+        
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        var geometry = new THREE.BoxGeometry();
+        var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        var animate = function () {
+            requestAnimationFrame(animate);
+
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
+
+        */
     }
 }
 
