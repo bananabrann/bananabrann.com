6854d263a3531eb26feacbdc8f5e7d9109fdc2d6
Pierson Brannan <plbrannan95@gmail.com>
2020-04-18 20:09:36 -0400

    Trim resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -23,17 +23,18 @@ class Resume extends React.Component {
         if (this.state.displayAlert) {
             alert = (
                 <Alert
-                    variant="warning"
+                    variant="info"
                     onClose={() => this.setState({ displayAlert: false })}
                     dismissible
                 >
                     <Alert.Heading>
-                        <i class="fas fa-wrench"></i> Style updates!
+                        <i class="fas fa-newspaper"></i> Want more info?
                     </Alert.Heading>
                     <p>
-                        This section is a Work In Progress, and so sorry
-                        if it looks like trash right now! Have feedback
-                        on the styling? Send me an email at plbrannan95@gmail.com
+                        The online resume is intended for a brief overview
+                        of my career, not as a subsitution for an actual
+                        resume. To view my resume in its fullest details, 
+                        please email me at <b>plbrannan95@gmail.com</b>
                     </p>
                 </Alert>
             );
@@ -53,45 +54,18 @@ class Resume extends React.Component {
                     </Row>
                     <Row>
                         <Col>
+                            {/* SECTION */}
                             <h3>Experience</h3>
                             <h4>Black Willow, Inc.</h4>
                             {/* TODO: Make an h5 */}
                             <h5>Web Developer - Frontend and API Gateway</h5>
                             <p>March 2020 to April 2020</p>
-                            <ul>
-                                <li><p>
-                                    - Reworked Black Willow's website using Pug, 
-                                    Bootstrap, and a little bit of SCSS, with 
-                                    compiling automation with shell scripts.
-                                </p></li>
-                                <li><p>
-                                    - Worked on a Kong API Gateway authentication 
-                                    plugin using Lua, Docker, and LDAP (Lightweight
-                                    Directory Access Protocol)
-                                </p></li>
-                            </ul>
-                            
+                            <p>React, SCSS, Kong API Gateway, Lua, LDAP</p>
                             <br />
                             
                             <h5>Software Developer - Automation and Qlik Sense</h5>
                             <p>April 2019 to March 2020</p>
-                            <ul>
-                                <li><p>
-                                    - Developed and integrated a custom front-end 
-                                    dark mode extension using CSS, JavaScript.
-                                </p></li>
-                                <li><p>
-                                    - Automated the data cleaning and handling of 
-                                    data prior to entering a Qlik application
-                                    with Bash, Python, and the Qlik Sense Data
-                                    Load Script to produce a clean, effective data
-                                    model.
-                                </p></li>
-                                <li><p>
-                                    Successfully deployed a Qlik application to
-                                    government servers.
-                                </p></li>
-                            </ul>
+                            <p>Python, Bash, Qlik Sense, SQL</p>
 
                             <br />
 
@@ -101,6 +75,18 @@ class Resume extends React.Component {
 
                             <br />
 
+                            {/* SECTION */}
+{/* 
+                            <h3>Certification</h3>
+                            <h4>AWS Cloud Practitioner</h4>
+                            <p>Cert #333333333</p>
+                            <br />
+                            <h4>Security+</h4>
+                            <p>Cert #444-FFF</p>
+
+                            <br /> */}
+
+                            {/* SECTION */}
                             <h3>Education</h3>
                             <h4>B.S. Software Engineering</h4>
                             <p>Arizona State University, October 2019 to Present</p>
