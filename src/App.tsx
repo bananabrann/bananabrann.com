5704ddfc2cc011625917568b04dd62108f538437
Pierson Brannan <plbrannan95@gmail.com>
2020-04-28 16:19:22 -0400

    Add Router capibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import * as React from 'react'
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { render } from 'react-dom';
+// import {Home, Foo, Bar} from './Pages';
 import Footer from "./Footer/Footer";
-import "./App.css";
+import Home from "./Home/Home";
+import "./App.scss";
 import "./styles/text.scss";
 
-function App() {
-  return (
-    <div className="App reset">
-      <Footer />
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/foo">Foo</Link>
+            <Link to="/bar">Bar</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
+
+render(<App />, document.getElementById('root'));
 
 export default App;
