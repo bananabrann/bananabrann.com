aa1eacdc930b3d9fce418d55be59d69cbc176135
Pierson Brannan <plbrannan95@gmail.com>
2020-04-30 12:01:36 -0400

    Working only in dist folder

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-bash build.sh
+# bash build.sh
 aws s3 rm s3://bananabrann.com --recursive
-aws s3 sync $DIR/build s3://bananabrann.com
+aws s3 sync $DIR/dist s3://bananabrann.com
 
 printf "\e[32m\e[1mdeploy.sh complete\033[0m\n"
