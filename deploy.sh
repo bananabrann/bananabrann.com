7cbb02e9ff308df1048e29024119ebb6d2ea43a6
Pierson Brannan <plbrannan95@gmail.com>
2020-04-30 08:34:04 -0400

    Cut down unuseful tech

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# GREEN = "\e[32m"
-# BOLD = "\e[1m"
-# CLEAR="\033[0m"
-
-DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-npm run-script build && aws s3 sync $DIR/build s3://bananabrann.com
-
-printf "\e[32m\e[1mdeploy.sh complete\033[0m\n"
